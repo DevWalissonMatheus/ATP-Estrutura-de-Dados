e9977724925185fcd1374ea883e4fc036aabeb8e
Note-KaTSuo <mwalisom9@gmail.com>
2024-05-15 22:36:20 -0300

    Adicionei um loop para perguntar se o usuario pretende comprar mais cartas

--- 2-questao.py
+++ 2-questao.py
@@ -9,13 +9,17 @@ def embaralhaCartas(naipes, numeros):
     return cartas
 
 def compraCartas(cartas):
-    qtd_cartas = int(input(('Qual a quantidade de cartas? ')))
-    if qtd_cartas > len(cartas):  # Verifica se há cartas suficientes
-        print("Não há cartas suficientes disponíveis. Embaralhando novamente.")
-        embaralhaCartas(naipes, numeros)  # Reembaralha as cartas
-    else:
-        for _ in range(qtd_cartas):
-            print(cartas.pop())
+    while True:
+        qtd_cartas = int(input(('Qual a quantidade de cartas? ')))
+        if qtd_cartas > len(cartas):  # Verifica se há cartas suficientes
+            print(f"Não há cartas suficientes disponíveis.\nRestam {len(cartas)} cartas.")
+            embaralhaCartas(naipes, numeros)  # Reembaralha as cartas
+        else:
+            for _ in range(qtd_cartas):
+                print(cartas.pop())
+        continuar = input('Deseja comprar mais cartas? (s/n): ')
+        if continuar.lower() != 's':
+            break
 
 naipes = ['Ouros', 'Espadas', 'Copas', 'Paus']
 numeros = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
