913dca768a28d0d1f561371d0e5a680c492f3c27
Note-KaTSuo <mwalisom9@gmail.com>
2024-05-15 22:11:48 -0300

    Adcionei uma verificação se as cartas restantes são suficientes para serem compradas

--- 2-questao.py
+++ 2-questao.py
@@ -4,14 +4,18 @@ def embaralhaCartas(naipes, numeros):
     cartas = []
     for naipe in naipes:
         for num in numeros:
-            cartas.append(f'{num} de {naipe}')
+            cartas.append(f'Comprou a carta: {num} de {naipe}')
     random.shuffle(cartas)
     return cartas
 
 def compraCartas(cartas):
-    qtd_cartas = int(input('Qual a quantidade de cartas?\n'))
-    for _ in range(qtd_cartas):
-        print(cartas.pop())
+    qtd_cartas = int(input(('Qual a quantidade de cartas? ')))
+    if qtd_cartas > len(cartas):  # Verifica se há cartas suficientes
+        print("Não há cartas suficientes disponíveis. Embaralhando novamente.")
+        embaralhaCartas(naipes, numeros)  # Reembaralha as cartas
+    else:
+        for _ in range(qtd_cartas):
+            print(cartas.pop())
 
 naipes = ['Ouros', 'Espadas', 'Copas', 'Paus']
 numeros = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
