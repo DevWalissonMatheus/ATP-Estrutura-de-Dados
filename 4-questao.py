8b8e226a51ba6a96104dd5b94a0010208024b173
Note-KaTSuo <mwalisom9@gmail.com>
2024-05-19 16:04:18 -0300

    Adicionando o código fonte dado pelo professor, para que passamos criar a função folhas

---
+++ 4-questao.py
@@ -0,0 +1,32 @@
+class BST:
+    def __init__(self, dado=None):
+        self.dado = dado
+        self.esquerda = None
+        self.direita = None
+    def inserir(self, dado):
+        if (self.dado == None):
+            self.dado = dado
+        else:
+            if (dado < self.dado):
+                if (self.esquerda):
+                    self.esquerda.inserir(dado)
+                else:
+                    self.esquerda = BST(dado)
+            else:
+                if(self.direita):
+                    self.direita.inserir(dado)
+                else:
+                    self.direita = BST(dado)
+    def folhas(self, lst):
+        return
+
+Teste = BST()
+Teste.inserir(7)
+Teste.inserir(4)
+Teste.inserir(9)
+Teste.inserir(0)
+Teste.inserir(5)
+Teste.inserir(8)
+Teste.inserir(13)
+
+print('Folhas: ', Teste.folhas([]))
