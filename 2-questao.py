ce16ce44c8ad7aa52ed39d7db65fd74f0c78070c
Note-KaTSuo <mwalisom9@gmail.com>
2024-05-20 11:40:42 -0300

    Agora o pergunta se o usuario quer comprar 3 cartas por vez ou escolher a quantidade desejada e adicionei comentarios nas novas linhas de código

--- 2-questao.py
+++ 2-questao.py
@@ -13,16 +13,19 @@ def embaralhaCartas(naipes, numeros):
 def compraCartas(cartas):
     while True: # Loop para perguntar se o usuário quer comprar mais cartas
         try: # Inicia um try para evitar erros
-            # Pergunta ao usuário quantas cartas quer comprar
-            qtd_cartas = int(input(('Insira a quantidade de cartas que deseja comprar? ')))
+            if modo == '1': # Se for escolhido o modo 1
+                # Pergunta a quantidade de cartas que deseja comprar
+                qtd_cartas = int(input('Quantas cartas deseja comprar? '))
+            else: # Se for escolhido o modo 2
+                # Compra 3 cartas por rodada
+                qtd_cartas = 3
             if qtd_cartas > len(cartas): # Verifica se ha cartas suficientes na pilha
-                # Se não houver mais cartas informa que a quantidade é menor que a quantidade
-                # que foi solicitada e informa a quantidade restante
+                # Se não houverem mais cartas informa que a quantidade é menor 
+                # que a quantidade solicitada e informa a quantidade restante
                 print(f"Não há cartas suficientes disponíveis.\nRestam apenas {len(cartas)} cartas.")
-                embaralhaCartas(naipes, numeros) # Reembaralha as cartas
             else: # Se a quantidade for suficiente compra as cartas
                 for _ in range(qtd_cartas): # Loop para retirar a quantidade desejada
-                    print(cartas.pop()) # Remove e imprime carta da pilha
+                    print(cartas.pop()) # Remove e imprime a carta da pilha
             if not cartas: 
                 # Se as cartas acabarem informa o usuário e encerra o jogo
                 print("As cartas acabaram.\nO jogo foi finalizado.")
@@ -39,5 +42,13 @@ def compraCartas(cartas):
 naipes = ['Ouros', 'Espadas', 'Copas', 'Paus'] # Define os naipes das cartas
 numeros = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'] # Define os números das cartas
 cartas = embaralhaCartas(naipes, numeros) # Embaralha as cartas e armazena na lista cartas
+# Pergunta ao usuário qual modo de jogo deseja
+modo = input('Escolha o modo de jogo:'
+             '\nDigite "1" para escolher a quantidade de cartas' 
+             '\nDigite "2" para comprar 3 cartas por rodada'
+             '\n>> ').strip() # .strip remove espaços em branco do início e do fim da string
+while modo not in ['1', '2']: # Se for inserido algo diferente de 1 ou 2
+    # Informa que é um modo inválido e refaz a pergunta
+    modo = input('Modo inválido. Escolha "1" ou "2": ').strip()
 
-compraCartas(cartas) # Chama a função para começar o jogo
+compraCartas(cartas) # Chama a função para começar o modo de jogo escolhido
